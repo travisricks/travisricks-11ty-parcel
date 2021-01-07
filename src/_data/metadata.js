8da1b08e34f88c8f71347c271032dfc188018d4b
Travis Ricks <travis@travisricks.com>
2021-01-07 21:23:41 +0900

    update metadata file

---
+++ src/_data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+  url: process.env.URL || "http://localhost:8080",
+  title: "Digital Home of Travis Ricks",
+  description: "Tokyo-based Front-end Software Engineer with experience in product management, client relations and team leadership",
+  author: {
+    name: "Travis Ricks",
+    email: "travis@travisricks.com"
+  },
+  img: "og-image.jpg",
+  githubUrl: "https://github.com/travisricks"
+};
